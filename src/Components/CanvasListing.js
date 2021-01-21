0218a43342153e2a2747857539e83dba17f82d23
DCartist <pbannerman13@gmail.com>
2021-01-21 00:52:41 -0500

    able to have canvas

--- src/Components/CanvasListing.js
+++ src/Components/CanvasListing.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
 import axios from 'axios'
+import parse from 'html-react-parser';
 
 class CanvasListing extends Component {
+    constructor(){
+        super()
+        this.state={
+            data:[],
+        }
+    }
     componentDidMount(){
-
+        console.log(`${process.env.REACT_APP_API_POST}`)
+		axios.get(`${process.env.REACT_APP_API_POST}`)
+		  .then(res => {
+		    console.log(res);
+            console.log(res.data);
+            this.setState({data: res.data})
+            let newString = JSON.parse(res.data[0].art);
+            console.log(newString)
+		  }).catch(err=>console.log(err))
     }
     render() {
         return (
             <div>
+<section className="flex">
+{this.state.data.map((artwork, index) => (
+        <div key={index} className="h-70 border border-black m-4">{parse(JSON.parse(artwork.art))}</div>
+    ))}</section>
+
                 
             </div>
         );
